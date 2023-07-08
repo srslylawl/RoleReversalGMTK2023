1704695861c4a33d4f212f97c2915d59cd5978be
Leonhard S <leonhard.samonig@gmx.net>
2023-07-08 14:21:41 +0200

    Scaleable Road
    Script is updated to accommodate scaling on the RoadProfile (you might want to scale x and y together)

--- Assets/Project/Scripts/SetupRoadCollision.cs
+++ Assets/Project/Scripts/SetupRoadCollision.cs
@@ -18,6 +18,8 @@ public class SetupRoadCollision : MonoBehaviour
 
         EdgeCollider2D edgeCol2D = GetComponent<EdgeCollider2D>();
 
+        edgeCol2D.enabled = false;
+
         Vector2[] points = edgeCol2D.points;
 
         foreach(Vector2 pos in points)
@@ -25,7 +27,8 @@ public class SetupRoadCollision : MonoBehaviour
             var col = Instantiate(roadColChild, transform.position, Quaternion.identity);
             col.transform.parent = this.transform;
 
-            col.transform.position += (Vector3)pos;
+            col.transform.localScale = new Vector3(col.transform.localScale.x * this.transform.localScale.x, col.transform.localScale.y, col.transform.localScale.z * this.transform.localScale.y);
+            col.transform.position += new Vector3(pos.x * this.transform.localScale.x, pos.y * this.transform.localScale.y, 0);
             col.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
         }
 
